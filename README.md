27901763f6ee5d9dca07265fb436fccfe9206e1e
bwilder95 <brentonwilder1995@gmail.com>
2021-05-21 16:06:11 -0700

    Update figure density

--- README.md
+++ README.md
@@ -12,11 +12,11 @@
 ## Data
 - Data was collected from https://openaltimetry.org/data/icesat2/
 - Data was compiled from 9 different passovers: 2018-11-29, 2019-02-28, 2019-05-30, 2019-08-26, 2019-11-27, 2020-05-27, 2020-08-26, 2020-11-24, and 2021-02-23
-- Much of the data recorded outlier canopy height greater than 35-meters. For this simple exercise the outlier data were converted to median canopy height.
+- Much of the data recorded outlier canopy height greater than 35-meters. For this simple exercise the outlier data were converted to median canopy height (15.5-meters).
 - Data included NaN or None data were dropped from the visualization
 
 ## Results
-- 2D representation of latitude (north/south) and canopy height (meters)
+- 2D representation of latitude and canopy height (meters)
 ![Canopy2D](https://github.com/bwilder95/ICESat2/blob/main/plots/Canopy_2D.png)
 
 - 3D representation of canopy height
--- icesat2_veg_height.py
+++ icesat2_veg_height.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
 
 # Concat ICESat csv files from data folder into one dataframe
 path = r'./data'                     
@@ -25,8 +26,8 @@ df = pd.concat(df_from_each_file, ignore_index=True)
 canopy_limit = 35 # meters
 canopy_median = 15.539093 # meters
 df['h_canopy'] = np.where((df.h_canopy > canopy_limit),canopy_median,df.h_canopy)
-df.dropna()
-boxplot = df.boxplot(column=['h_canopy'])
+df = df.dropna()
+#boxplot = df.boxplot(column=['h_canopy'])
 
 # Plot 3-D plot to represent the trees
 x = df.longitude
@@ -41,22 +42,31 @@ ax = plt.axes(projection ="3d")
 ax.grid(b = True, color ='grey',
         linestyle ='-.', linewidth = 0.3,
         alpha = 0.2)
-my_cmap = plt.get_cmap('hsv')
-sctt = ax.scatter3D(x, y, dz,
-                    alpha = 0.8,
-                    c = (dz),
-                    cmap = my_cmap,
-                    marker ='^')
+bar = ax.bar3d(x, y,z,dx,dy,dz, color='green')
 ax.set_xlabel('longitude', fontweight ='bold')
 ax.set_ylabel('latitude', fontweight ='bold')
 ax.set_zlabel('canopy height (m)', fontweight ='bold')
-fig.colorbar(sctt, ax = ax, shrink = 0.5, aspect = 5)
 ax.set_zlim(0,40)
-ax.view_init(12,-73)
+ax.view_init(23,-33)
 
-# Plot 2-D profile with respect to latitude
-ax2 = df.plot.scatter(x='latitude',
-                      y='h_canopy',
-                      c='h_canopy',
-                      colormap=my_cmap,s=10)
+###############################################################################
+###############################################################################
+###############################################################################
+
+# Plot 2-D profile with respect to latitude with point density for 
+# Calculate the point density
+x = df['latitude'].to_numpy()
+y = df['h_canopy'].to_numpy()
+xy = np.vstack([x,y])
+z = gaussian_kde(xy)(xy)
+
+# Sort the points by density, so that the densest points are plotted last
+idx = z.argsort()
+x, y, z = x[idx], y[idx], z[idx]
+
+fig, ax = plt.subplots()
+density = ax.scatter(x, y, c=z, s=50)
+fig.colorbar(density, label='Density of points')
+ax.set_xlabel('latitude')
+ax.set_ylabel('canopy height (m)')
 plt.show()
